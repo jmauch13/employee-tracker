5aa44a8e273d7f12c1cbc8814e0f2b0e2007d08e
Jennie Mauch <jenniemauch@me.com>
2022-05-13 10:23:04 -0400

    updated index.js

--- db/schema.sql
+++ db/schema.sql
@@ -1,22 +1,22 @@
-DROP DATABASE IF EXISTS inventory_db;
-CREATE DATABASE inventory_db;
+DROP DATABASE IF EXISTS employee_tracker;
+CREATE DATABASE employee_tracker;
 
-USE inventory_db;
+USE employee_tracker;
 
 CREATE TABLE department(
-    id INT PRIMARY KEY,
-    department_name VARCHAR(30),
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    department_name VARCHAR(30)
 );
 
 CREATE TABLE role(
-    id INT PRIMARY KEY,
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30),
     salary DECIMAL,
     department_id INT
 );
 
 CREATE TABLE employee(
-    id INT PRIMARY KEY,
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30),
     last_name VARCHAR(30),
     role_id INT,
